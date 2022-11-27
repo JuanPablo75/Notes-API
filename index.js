698694a86b174146bde328c977fcd09dc88d4aab
Juan Pablo Alu Aguerre <85246010+JuanPablo75@users.noreply.github.com>
2022-11-27 11:51:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const logger = require('./loggerMiddleware')
 
-//app.use(cors())
+app.use(cors())
 app.use(express.json())
 
 app.use(logger)
